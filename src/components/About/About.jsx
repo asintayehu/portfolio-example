2bf22bf83a7ac4625f69732838a92be76abf561d
Aaron <asintayehu@proton.me>
2023-01-08 12:32:04 -0500

    updated nav bar, added some images, updated about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import "./about.css"
+// import ME from "../../assets/kdot-real.png"
+// import ME from "../../assets/Kendrick.png"
+import ME from "../../assets/kdot-hs.jpg"
+import {BsFillAwardFill} from 'react-icons/bs'
+import {FaUsers} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+      
+      <div className="container about__container">
+
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image"/>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+
+            <article className="about__card">
+              <BsFillAwardFill className="about__icon"/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FaUsers className="about__icon"/>
+              <h5>Clients</h5>
+              <small>300+ Clients Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon"/>
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+
+          </div>
+          <div className="about__text-container">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, sapiente soluta unde tenetur facere quis ad repudiandae tempore pariatur architecto commodi explicabo perspiciatis, totam, odio qui rem maxime iure? Eveniet!</p>
+              <br />
+              <a href="#contact" className='btn btn-primary'> Let's Talk!</a>
+            </div>
+        </div>
+        
+      </div>
+
+      </section>
   )
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import "./nav.css"
+import {FaHome} from 'react-icons/fa'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsBriefcaseFill} from 'react-icons/bs'
+import {RiServiceLine} from 'react-icons/ri'
+import {AiOutlineMessage} from 'react-icons/ai'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#'); //brings us to top of page
+
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+
+      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FaHome/></a>
+
+      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBriefcaseFill/></a>
+
+      <a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/></a>
+
+    </nav>
   )
 }
 
